780eb133c95aab358f71f0fa59e1d83698b03540
Eric Espinoza <espinoza.eric@gmail.com>
2024-06-26 22:30:02 -0400

    actividad 4 modulo 2

---
+++ src/main/java/com/example/myproject/App.java
@@ -0,0 +1,20 @@
+package com.example.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "saludos del Grupo 6" );
+        System.out.println( "Integrantes:" );
+        System.out.println( "    - Angel Smith" );
+        System.out.println( "    - Edgardo Riveros" );
+        System.out.println( "    - Eric Espinoza" );
+        System.out.println( "    - Marcela Catalan" );
+
+    }
+}
